4d08c4fc8f15318deb8ca324f63c57b2b4eb2cf4
Ornella Meneghini <83933025+MeneghiniOrnella@users.noreply.github.com>
2021-10-21 20:15:18 -0300

    Update script

--- script.js
+++ script.js
@@ -1,105 +1,146 @@
-let preguntaActual = 0;
-let rtaCorrecta = 0;
-let rtaNo = 0;
+let rightAnswer;
 
-const cuestionario = [
+let currentQuestionIndex = 0;
+let rightAnswers = 0;
+let wrongAnswers = 0;
+
+// Tiempo limite;
+let timeInterval;
+let time = 10;
+
+const cuestionary = [
     {
-        'pregunta': 'Dónde se creó el taekwondo?',
-        // 'img': './img/tkd.png',
-        'respuestas': ['Corea','China','Japón','Argentina'],
+        'question': 'Dónde se creó el taekwondo?',
+        // Imagenes:
+        // 'img': './media/tkd.png',
+        // Audio:
+        // 'audio': './media/tkd.mp3',
+        'answers': ['Corea','China','Japón','Argentina'],
     },
     {
-        'pregunta': 'Cuándo se creó el taekwondo?',
-        'respuestas': ['1955','1945','1965','1966'],
+        'question': 'Cuándo se creó el taekwondo?',
+        'answers': ['1955','1945','1965','1966'],
     },
     {
-        'pregunta': 'Cuando se creó la Escuela Dung San?',
-        'respuestas': ['2010','2006','2008','2012'],
+        'question': 'Cuando se creó la Escuela Dung San?',
+        'answers': ['2010','2006','2008','2012'],
     },
     {
-        'pregunta': 'Dónde se creó el estilo de taekwondo ITF?',
-        'respuestas': ['Corea del Norte','Corea del Este','Corea del Oeste','Corea del Sur']
+        'question': 'Dónde se creó el estilo de taekwondo ITF?',
+        'answers': ['Corea del Norte','Corea del Este','Corea del Oeste','Corea del Sur']
     },
     {
-        'pregunta': 'Qué significa Dung San?',
-        'respuestas': ['escalar montañas','montañas','caminar montañas','saltar montañas']
+        'question': 'Qué significa Dung San?',
+        'answers': ['escalar montañas','montañas','caminar montañas','saltar montañas']
     },
     {
-        'pregunta': 'Cuándo se creó A.CE.T.?',
-        'respuestas': ['2002','1960','2011','1999']
+        'question': 'Cuándo se creó A.CE.T.?',
+        'answers': ['2002','1960','2011','1999']
     },
     {
-        'pregunta': 'Cuál es la traducción de 태권도 o 跆拳道?',
-        'respuestas': ['Taekwondo','ITF','Dung San','WTF']
+        'question': 'Cuál es la traducción de 태권도 o 跆拳道?',
+        'answers': ['Taekwondo','ITF','Dung San','WTF']
     },
     {
-        'pregunta': 'Qué estilo de taekwondo practicas?',
-        'respuestas': ['ITF: International Taekwondo Federation','IT: International Taekwondo','WT: World Taekwondo','WTF: World Taekwondo Federation']
+        'question': 'Qué estilo de taekwondo practicas?',
+        'answers': ['ITF: International Taekwondo Federation','IT: International Taekwondo','WT: World Taekwondo','WTF: World Taekwondo Federation']
     },
     {
-        'pregunta': 'Qué representa el color amarillo?',
-        'respuestas': ['tierra donde se planta la semilla','semilla donde sale la planta','Sol','flor amarilla']
+        'question': 'Qué representa el color amarillo?',
+        'answers': ['tierra donde se planta la semilla','semilla donde sale la planta','Sol','flor amarilla']
     },
     {
-        'pregunta': 'Cuántos tules o formas existen en total?',
-        'respuestas': ['15','10','9','20']
+        'question': 'Cuántos tules o formas existen en total?',
+        'answers': ['15','10','9','20']
     },
     {
-        'pregunta': 'Quién es el presidente de la Escuela Dung San?',
-        'respuestas': ['Sabonim Daniel Reartes IV dan','Sabonim Claudio Defelice VI dan','Sagiumnim Héctor Hernandez VIII dan','Sabonim Federico Hernandez VI dan']
+        'question': 'Quién es el presidente de la Escuela Dung San?',
+        'answers': ['Sabonim Daniel Reartes IV dan','Sabonim Claudio Defelice VI dan','Sagiumnim Héctor Hernandez VIII dan','Sabonim Federico Hernandez VI dan']
     },
     {
-        'pregunta': 'Cúando se trajo el taekwondo a Argentina?',
-        'respuestas': ['1966','1955','1965','1967']
+        'question': 'Cúando se trajo el taekwondo a Argentina?',
+        'answers': ['1966','1955','1965','1967']
     },
     {
-        'pregunta': 'A qué asociación de taekwondo perteneces?',
-        'respuestas': ['A.CE.T.','Yom Chi','Dung San','U.E.N.A.T.']
+        'question': 'A qué asociación de taekwondo perteneces?',
+        'answers': ['A.CE.T.','Yom Chi','Dung San','U.E.N.A.T.']
     },
     {
-        'pregunta': 'Quién es el presidente de A.CE.T.?',
-        'respuestas': ['Sagiumnim Héctor Hernandez VIII dan','Sabonim Claudio Defelice VI dan','Sabonim Daniel Reartes IV dan','Sabonim Federico Hernandez VI dan']
+        'question': 'Quién es el presidente de A.CE.T.?',
+        'answers': ['Sagiumnim Héctor Hernandez VIII dan','Sabonim Claudio Defelice VI dan','Sabonim Daniel Reartes IV dan','Sabonim Federico Hernandez VI dan']
     },
     {
-        'pregunta': 'Qué significa chumok?',
-        'respuestas': ['Golpe de puño','Nudillo','Mano','Puño']
+        'question': 'Qué significa chumok?',
+        'answers': ['Golpe de puño','Nudillo','Mano','Puño']
     },
     {
-        'pregunta': 'Qué significa dojang?',
-        'respuestas': ['Habitación','Lugar de práctica','Escuela','Escudo']
+        'question': 'Qué significa dojang?',
+        'answers': ['Habitación','Lugar de práctica','Escuela','Escudo']
     }
 ];
 
-let printHTMLpregunta = (i) => {
+let printHtmlQuestion = (i) => {
     preguntaActualIndex++;
     const q = cuestionario[i];
     let a = q.respuestas;
     rtaCorrecta = a[0];
-    const htmlRespuestasArray = a.map(currentA => 
-        `<p>
+    
+    // Metodo random:
+    a = a.sort((a,b) => Math.floor(Math.random()*3)-1);
+
+    // Audio:
+    /*let audio = new Audio('q.audio');
+    audio.play();*/
+
+    const htmlAnswersArray = a.map(currentA => 
+        `<p class="answer">
             <button onclick="corregir('${currentA}')",this>X</button>
             <span>${currentA}</span>
         </p>`
     );
-    const htmlRespuestas = htmlRespuestasArray.join(' ');
-    let htmlCuestionarioCode = `<p class="respuestas">${q.pregunta}</p><div>${htmlRespuestas}</div>`;
-    document.querySelector('.pregunta').innerHTML = htmlCuestionarioCode;
+
+    const htmlAnswers = htmlAnsweraArray.join(' ');
+    // Imagenes:
+    //let htmlCuestionarioCode = `<p class="respuestas">${q.pregunta}</p><img src="${q.img}"><div>${htmlRespuestas}</div>`;
+        let htmlQuestionCode = 
+            `<p>${q.question}</p>
+            <div>${htmlAnswers}</div>`;
+        document.querySelector('.question').innerHTML = htmlQuestionCode;
+
+    // Tiempo limite:
+    time = 10;
+    document.querySelector('.time').innerHTML = time; 
+    clearInterval(timeInterval);
+    timeInterval = setInterval(() => {
+        time --;
+        if(time==0){
+            alert("No respondiste a tiempo");
+            clearInterval(timeInterval);
+        }else{
+           document.querySelector('.time').innerHTML = time; 
+        }
+    },100000);
+
+    const corregir = (respuestas, obj) =>{
+        document.querySelectorAll('.respuestas').forEach(a => a.classList.remove('right','wrong'));
+        const parenP = obj.parentNode;
+        if(answer == rightAnswer){
+            parenP.classList.add('right');
+            rightAnswer++;
+            document.querySelector('.rightCounter').innerHTML = rightCounter;
+        }else{
+            parenP.classList.add('wrong');
+            wrongAnswers++;
+            document.querySelector('.wrongCounter').innerHTML = wrongAnswers;
+    }
+};
+
+const nextQuestion = _ => {
+
 }
 
-const corregir = (respuestas, obj) =>{
-    document.querySelectorAll('.respuestas').forEach(a => a.classList.remove('right','wrong'));
-    const parenP = obj.parentNode;
-    if(respuestas == rtaCorrecta){
-        parenP.classList.add('right');
-        rtaCorrecta++;
-        document.querySelector('.rightContador').innerHTML = rtaCorrecta;
-    }else{
-        parenP.classList.add('wrong');
-        rtaNo++;
-        document.querySelector('.wrongContador').innerHTML = rtaNo;
-    };
-}
-
-printHTMLpregunta(0);
-
-
+const start = _ =>{
+    printHtmlQuestion(0);
+    documet.getElementById('.start').style.display = 'none';
+    documet.getElementById('.everything').style.display = 'block';
+};
