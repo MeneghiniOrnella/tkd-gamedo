d932ade4d602b2408916c2fedd7bbe3f92490408
Ornella Meneghini <meneghini.ornella@gmail.com>
2023-04-11 19:07:57 -0300

    Update but it isn't working easyC.js & hardC.js

--- app.js
+++ app.js
@@ -1,119 +1,125 @@
-import * from "./cuestionary.js";
-let cuestionary = [], rightAnswer, timeLimit, numberOfQuestions = 10, currentQuestionIndex = 0, rightAnswers = 0, wrongAnswers = 0, answers;
-// const easyCuestionary = [
-//     {
-//         'question': 'Dónde se creó el taekwondo?',
-//         /* 'img': './media/tkd.png',
-//         'audio': './media/tkd.mp3', */
-//         'answers': ['Corea','China','Japón','Argentina']
-//     },
-//     {
-//         'question': 'Cuando se creó la Escuela Dung San?',
-//         'answers': ['2010','2006','2008','2012']
-//     },
-//     {
-//         'question': 'Dónde se creó el estilo de taekwondo ITF?',
-//         'answers': ['Corea del Norte','Corea del Este','Corea del Oeste','Corea del Sur']
-//     },
-//     {
-//         'question': 'Qué significa Dung San?',
-//         'answers': ['escalar montañas','montañas','caminar montañas','saltar montañas']
-//     },
-//     {
-//         'question': 'Cuál es la traducción de 태권도 o 跆拳道?',
-//         'answers': ['Taekwondo','ITF','Dung San','WTF']
-//     },
-//     {
-//         'question': 'Con qué parte del cuerpo pega la patada de costado?',
-//         'answers': ['Talón','Empeine','Metatarso','Dedos del pie']
-//     },
-//     {
-//         'question': 'Qué estilo de taekwondo practicas?',
-//         'answers': ['ITF: International Taekwondo Federation','IT: International Taekwondo','WT: World Taekwondo','WTF: World Taekwondo Federation']
-//     },
-//     {
-//         'question': 'Cuándo se creó el taekwondo?',
-//         'answers': ['1955','1945','1965','1966']
-//     },
-//     {
-//         'question': 'Qué representa el color amarillo?',
-//         'answers': ['tierra donde se planta la semilla','semilla donde sale la planta','Sol','flor amarilla']
-//     },
-//     {
-//         'question': 'Cuántos tules o formas existen en total?',
-//         'answers': ['24','10','9','15']
-//     },
-//     {
-//         'question': 'Cúando se trajo el taekwondo a Argentina?',
-//         'answers': ['1967','1955','1965','1967']
-//     },
-//     {
-//         'question': 'Cuántos competidores hay en un cuadrilatero?',
-//         'answers': ['2','1','3','5']
-//     },
-//     {
-//         'question': 'Cúal es la distribución del peso de la posición en L?',
-//         'answers': ['70%-30%','90%-10%','50%-50%','80%-20%']
-//     },
-//     {
-//         'question': 'Cómo se dice profesor en coreano?',
-//         'answers': ['sabonim','sabom','bu sabonim','tae']
-//     }
-// ];
-// const hardCuestionary = [
-//     {
-//         'question': 'Qué significa chumok?',
-//         'answers': ['Golpe de puño','Nudillo','Mano','Puño']
-//     },
-//     {
-//         'question': 'Qué significa dojang?',
-//         'answers': ['Habitación','Lugar de práctica','Escuela','Escudo']
-//     },
-//     {
-//         'question': 'Cúal no es una advertencia en combate?',
-//         'answers': ['Golpe al pecho con salto y giro','Salida del cuadrilatero','Golpe a la espalda','Exceso de contacto']
-//     },
-//     {
-//         'question': 'Cúal no es una prenda del dobook?',
-//         'answers': ['Remera','Chaqueta','Cinturón','Pantalón']
-//     },
-//     {
-//         'question': 'Qué significa maki?',
-//         'answers': ['defensa','ataque','giro','guardia']
-//     },
-//     {
-//         'question': 'Cómo se dice posición en coreano?',
-//         'answers': ['sogui','niunja','annun','debi']
-//     },
-//     {
-//         'question': 'Cómo se dice 10 en coreano?',
-//         'answers': ['yol','hana','dul','net']
-//     },
-//     {
-//         'question': 'Cómo se dice 3 en coreano?',
-//         'answers': ['set','dasot','ilgob','ahob']
-//     },
-//     {
-//         'question': 'Qué significa hong?',
-//         'answers': ['rojo','competidor','referi','azul']
-//     },
-//     {
-//         'question': 'Cómo se dice azul en coreano?',
-//         'answers': ['chong','rojo','competidor','referi']
-//     },
-//     {
-//         'question': 'Cómo se dice mano en coreano?',
-//         'answers': ['son','palmok','sonnal','chumok']
-//     },
-//     {
-//         'question': 'Cómo se dice patada en coreano?',
-//         'answers': ['chagui','sogui','chirigui','tae']
-//     },
-//     {
-//         'question': 'Cómo se dice rodilla en coreano?',
-//         'answers': ['morup','chagui','palkup','tae']
-//     }
-// ];
+let numberOfQuestions = 10;
+let currentQuestionIndex = 0;
+let rightAnswer;
+let timeLimit;
+let rightAnswers = 0;
+let wrongAnswers = 0;
+let answers;
+const easyCuestionary = [
+    {
+        'question': 'Dónde se creó el taekwondo?',
+        /* 'img': './media/tkd.png',
+        'audio': './media/tkd.mp3', */
+        'answers': ['Corea','China','Japón','Argentina']
+    },
+    {
+        'question': 'Cuando se creó la Escuela Dung San?',
+        'answers': ['2010','2006','2008','2012']
+    },
+    {
+        'question': 'Dónde se creó el estilo de taekwondo ITF?',
+        'answers': ['Corea del Norte','Corea del Este','Corea del Oeste','Corea del Sur']
+    },
+    {
+        'question': 'Qué significa Dung San?',
+        'answers': ['escalar montañas','montañas','caminar montañas','saltar montañas']
+    },
+    {
+        'question': 'Cuál es la traducción de 태권도 o 跆拳道?',
+        'answers': ['Taekwondo','ITF','Dung San','WTF']
+    },
+    {
+        'question': 'Con qué parte del cuerpo pega la patada de costado?',
+        'answers': ['Talón','Empeine','Metatarso','Dedos del pie']
+    },
+    {
+        'question': 'Qué estilo de taekwondo practicas?',
+        'answers': ['ITF: International Taekwondo Federation','IT: International Taekwondo','WT: World Taekwondo','WTF: World Taekwondo Federation']
+    },
+    {
+        'question': 'Cuándo se creó el taekwondo?',
+        'answers': ['1955','1945','1965','1966']
+    },
+    {
+        'question': 'Qué representa el color amarillo?',
+        'answers': ['tierra donde se planta la semilla','semilla donde sale la planta','Sol','flor amarilla']
+    },
+    {
+        'question': 'Cuántos tules o formas existen en total?',
+        'answers': ['24','10','9','15']
+    },
+    {
+        'question': 'Cúando se trajo el taekwondo a Argentina?',
+        'answers': ['1967','1955','1965','1967']
+    },
+    {
+        'question': 'Cuántos competidores hay en un cuadrilatero?',
+        'answers': ['2','1','3','5']
+    },
+    {
+        'question': 'Cúal es la distribución del peso de la posición en L?',
+        'answers': ['70%-30%','90%-10%','50%-50%','80%-20%']
+    },
+    {
+        'question': 'Cómo se dice profesor en coreano?',
+        'answers': ['sabonim','sabom','bu sabonim','tae']
+    }
+];
+const hardCuestionary = [
+    {
+        'question': 'Qué significa chumok?',
+        'answers': ['Golpe de puño','Nudillo','Mano','Puño']
+    },
+    {
+        'question': 'Qué significa dojang?',
+        'answers': ['Habitación','Lugar de práctica','Escuela','Escudo']
+    },
+    {
+        'question': 'Cúal no es una advertencia en combate?',
+        'answers': ['Golpe al pecho con salto y giro','Salida del cuadrilatero','Golpe a la espalda','Exceso de contacto']
+    },
+    {
+        'question': 'Cúal no es una prenda del dobook?',
+        'answers': ['Remera','Chaqueta','Cinturón','Pantalón']
+    },
+    {
+        'question': 'Qué significa maki?',
+        'answers': ['defensa','ataque','giro','guardia']
+    },
+    {
+        'question': 'Cómo se dice posición en coreano?',
+        'answers': ['sogui','niunja','annun','debi']
+    },
+    {
+        'question': 'Cómo se dice 10 en coreano?',
+        'answers': ['yol','hana','dul','net']
+    },
+    {
+        'question': 'Cómo se dice 3 en coreano?',
+        'answers': ['set','dasot','ilgob','ahob']
+    },
+    {
+        'question': 'Qué significa hong?',
+        'answers': ['rojo','competidor','referi','azul']
+    },
+    {
+        'question': 'Cómo se dice azul en coreano?',
+        'answers': ['chong','rojo','competidor','referi']
+    },
+    {
+        'question': 'Cómo se dice mano en coreano?',
+        'answers': ['son','palmok','sonnal','chumok']
+    },
+    {
+        'question': 'Cómo se dice patada en coreano?',
+        'answers': ['chagui','sogui','chirigui','tae']
+    },
+    {
+        'question': 'Cómo se dice rodilla en coreano?',
+        'answers': ['morup','chagui','palkup','tae']
+    }
+];
+let cuestionary = [];
 
 document.querySelector('#easyBtn').addEventListener('click', () => {
     cuestionary = easyCuestionary;
---
+++ hardCuestionary.js
@@ -0,0 +1,70 @@
+export const hardCuestionary = [
+    {
+        'question': 'Qué significa chumok?',
+        'answers': ['Golpe de puño','Nudillo','Mano','Puño']
+    },
+    {
+        'question': 'Qué tips de ejercicios de entrada en calor existe?',
+        'answers': ['Aerobicos y Anabolicos','Dinamicos y Estaticas']
+    },
+    {
+        'question': 'Qué significa dojang?',
+        'answers': ['Habitación','Lugar de práctica','Escuela','Escudo']
+    },
+    {
+        'question': 'Cúal no es una advertencia en combate?',
+        'answers': ['Golpe al pecho con salto y giro','Salida del cuadrilatero','Golpe a la espalda','Exceso de contacto']
+    },
+    {
+        'question': 'Cúal no es una prenda del dobook?',
+        'answers': ['Remera','Chaqueta','Cinturón','Pantalón']
+    },
+    {
+        'question': 'Qué significa maki?',
+        'answers': ['defensa','ataque','giro','guardia']
+    },
+    {
+        'question': 'Cómo se dice posición en coreano?',
+        'answers': ['sogui','niunja','annun','debi']
+    },
+    {
+        'question': 'Cómo se dice 10 en coreano?',
+        'answers': ['yol','hana','dul','net']
+    },
+    {
+        'question': 'Cómo se dice 3 en coreano?',
+        'answers': ['set','dasot','ilgob','ahob']
+    },
+    {
+        'question': 'Qué significa hong?',
+        'answers': ['rojo','competidor','referi','azul']
+    },
+    {
+        'question': 'Cómo se dice azul en coreano?',
+        'answers': ['chong','rojo','competidor','referi']
+    },
+    {
+        'question': 'Cómo se dice mano en coreano?',
+        'answers': ['son','palmok','sonnal','chumok']
+    },
+    {
+        'question': 'Cómo se dice patada en coreano?',
+        'answers': ['chagui','sogui','chirigui','tae']
+    },
+    {
+        'question': 'Cómo se dice rodilla en coreano?',
+        'answers': ['morup','chagui','palkup','tae']
+    },
+    {
+        'question': 'Quienes trajeron el taekwondo a la Argentina?',
+        'answers': ['Han CHang Kim, Nam Sun Choi, Kwan Duk Chung','Choi Hong Hi, Nam Sun Choi, Kwan Duk Chung','Choi Hong Hi']
+    },
+    {
+        'question': 'Desde cuando se implementaron las puntas en los cinturones?',
+        'answers': ['1980','2002','1955','1967']
+    },
+    {
+        'question': 'Cuál fue el ultimo tul creado?',
+        'answers': ['juche en 1963','choong moo en 2003','juche en 1999','choong moo en 1963']
+    }
+];
